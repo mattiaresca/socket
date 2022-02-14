50fc104a2417b3205529cc2bd880d95d6152f84a
mattiaresca <92153287+mattiaresca@users.noreply.github.com>
2022-02-14 21:24:49 +0100

    fyyig

--- pagella_server.py
+++ pagella_server.py
@@ -2,7 +2,7 @@ import socket
 import json
 
 HOST="127.0.0.1"
-PORT=65432
+PORT=20019
 voti = {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7.5,4),("Storia",7.5,4),("Geografia",5,7)],
            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
            'Nicola Spina':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]}
